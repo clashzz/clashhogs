96c7cbc4499f837d77b6b8ff9b8109738dac50ac
zz <ziqizhang.email@gmail.com>
2022-04-13 23:43:53 +0100

    refactorisation-v2

--- src/clashhogs/bot.py
+++ src/clashhogs/bot.py
@@ -316,8 +316,8 @@ async def clanwar(inter, clantag: str, from_date: str, to_date=None):
             to_date = datetime.datetime.now()
             await inter.channel.send(
                     "End date not provided or does not conform to the format dd/mm/yyyy, using today's date as the end date")
-        war_miss, cwl_miss, war_overview, war_plot, summary = prepare_wardigest(from_date, to_date, clantag,
-                                                                                clanwatch._name)
+        war_miss, cwl_miss, war_overview, war_plot, summary = \
+            bot_functions.prepare_wardigest(from_date, to_date, clantag,clanwatch._name, database)
         if war_miss is None or cwl_miss is None or war_overview is None or war_plot is None:
             await inter.response.send_message("Not enough war data for {}, {}. Please try again later after a few wars.".format(clantag, clanwatch._name))
             return
@@ -679,9 +679,9 @@ async def waw_view(inter, option: str = commands.Param(choices={"clan": "-lc",
                 prev_war = database.MEM_current_cwl_wars[clantag][1]
                 members = prev_war.members
                 attacks = prev_war.attacks
-                missed_attacks, registered = register_war_attacks(members, attacks, prev_war, clantag,
+                missed_attacks, registered = bot_functions.register_war_attacks(members, attacks, prev_war, clantag,
                                                                   prev_war.type,
-                                                                  1)
+                                                                  1, database)
                 if registered:
                     log.info(
                         "\tCredits registered for: {}. Missed attacks: {}".format(clantag, missed_attacks))
@@ -690,7 +690,7 @@ async def waw_view(inter, option: str = commands.Param(choices={"clan": "-lc",
                         "\tCredits not registered for: {}, something wrong... ".format(clantag,
                                                                                        missed_attacks))
 
-                channel, misses = send_missed_attacks(missed_attacks, clantag)
+                channel, misses = bot_functions.send_missed_attacks(missed_attacks, clantag, database, bot)
                 if channel is not None and misses is not None:
                     await channel.send(misses)
 
@@ -980,7 +980,8 @@ async def current_war_state(old_war: coc.ClanWar, new_war: coc.ClanWar):
 
             members = old_war.members
             attacks = old_war.attacks
-            missed_attacks, registered = register_war_attacks(members, attacks, old_war, clan_home, type, total_attacks)
+            missed_attacks, registered = bot_functions.\
+                register_war_attacks(members, attacks, old_war, clan_home, type, total_attacks, database)
             if registered:
                 log.info(
                     "\tCredits registered for: {}. Missed attacks: {}".format(old_war.clan, missed_attacks))
@@ -988,7 +989,7 @@ async def current_war_state(old_war: coc.ClanWar, new_war: coc.ClanWar):
                 log.info(
                     "\tCredits not registered for: {}, something wrong... ".format(old_war.clan, missed_attacks))
 
-            channel, misses = send_missed_attacks(missed_attacks, clan_home.tag)
+            channel, misses = bot_functions.send_missed_attacks(missed_attacks, clan_home.tag, database, bot)
             if channel is not None and misses is not None:
                 await channel.send(misses)
 
@@ -1015,8 +1016,8 @@ async def current_war_stats(attack, war):
             prev_war = database.MEM_current_cwl_wars[attacker_clan.tag][1]
             members = prev_war.members
             attacks = prev_war.attacks
-            missed_attacks, registered = register_war_attacks(members, attacks, prev_war, attacker_clan, prev_war.type,
-                                                              1)
+            missed_attacks, registered = bot_functions.register_war_attacks(members, attacks, prev_war, attacker_clan, prev_war.type,
+                                                              1, database)
             if registered:
                 log.info(
                     "\tCredits registered for: {}. Missed attacks: {}".format(attacker_clan.tag, missed_attacks))
@@ -1024,7 +1025,7 @@ async def current_war_stats(attack, war):
                 log.info(
                     "\tCredits not registered for: {}, something wrong... ".format(attacker_clan.tag, missed_attacks))
 
-            channel, misses = send_missed_attacks(missed_attacks, attacker_clan.tag)
+            channel, misses = bot_functions.send_missed_attacks(missed_attacks, attacker_clan.tag, database, bot)
             if channel is not None and misses is not None:
                 await channel.send(misses)
 
@@ -1035,7 +1036,8 @@ async def current_war_stats(attack, war):
 @coc_client.event
 @coc.ClanEvents.member_join()
 async def on_clan_member_join(member, clan):
-    messages, tochannel = log_member_movement(member.tag, member.name, clan.name, clan.tag, "joined")
+    messages, tochannel = bot_functions.log_member_movement(member.tag, member.name, clan.name, clan.tag, "joined",
+                                                            database,bot)
     if len(messages) > 0:
         for m in messages:
             await tochannel.send(m)
@@ -1044,7 +1046,9 @@ async def on_clan_member_join(member, clan):
 @coc_client.event
 @coc.ClanEvents.member_leave()
 async def on_clan_member_leave(member, clan):
-    messages, tochannel = log_member_movement(member.tag, member.name, clan.name, clan.tag, "left")
+    print("left")
+    messages, tochannel = bot_functions.log_member_movement(member.tag, member.name, clan.name, clan.tag, "left",
+                                                            database,bot)
     if len(messages) > 0:
         for m in messages:
             await tochannel.send(m)
@@ -1052,138 +1056,6 @@ async def on_clan_member_leave(member, clan):
 @coc.ClientEvents.event_error()
 async def callback(exception):
     log.error("Events had an error: {}".format(exception), exc_info=exception)
-
-def log_member_movement(membertag, membername, clanname, clantag, join_or_left: str):
-    messages = []
-    to_channel = None
-    if clantag in database.MEM_mappings_clanwatch.keys():
-        clanwatch = database.MEM_mappings_clanwatch[clantag]
-        guild = bot.get_guild(clanwatch._guildid)
-        if guild is not None and clanwatch._channel_clansummary is not None:
-            channel_id = dataformatter.parse_channel_id(clanwatch._channel_clansummary)
-            channel = disnake.utils.get(guild.channels, id=channel_id)
-            if channel is not None:
-                emoji = ""
-                if join_or_left == 'joined':
-                    emoji = ":green_circle:"
-                else:
-                    emoji = ":red_circle:"
-                to_channel = channel
-                messages.append("{} **{}, {}** has {} the clan **{}**".format(emoji, membername,
-                                                                              membertag, join_or_left,
-                                                                              clanname))
-                member_name_variants = util.generate_variants(membername)
-                guild_member_names = {}
-                for m in guild.members:
-                    if not m.bot:
-                        guild_member_names[m.display_name] = util.generate_variants(m.display_name)
-                matching = util.find_overlap(member_name_variants, guild_member_names)
-
-                if len(matching) > 0:
-                    msg = "Please check if the member's discord roles need changing. " \
-                          "Possible discord name matches found:\n"
-                    for m in matching:
-                        msg += "\t\t" + m + "\n"
-                    messages.append(msg)
-                else:
-                    messages.append("I can't find similar discord names. Please check manually.")
-
-                # check for blacklist
-                if join_or_left == "joined":
-                    entries = database.show_blacklist(clanwatch._guildid, membertag)
-                    records = dataformatter.format_blacklist(entries)
-                    if len(records) > 0:
-                        msg = ":warning: **WARNING** this member is currently on our blacklist:\n"
-                        messages.append(msg)
-                        messages.extend(records)
-    return messages, to_channel
-
-def register_war_attacks(members: list, attacks: list, old_war, clan_home, type, total_attacks):
-    attack_data = {}
-    for m in members:
-        if not m.is_opponent:
-            attack_data[(m.name, m.tag)] = []
-
-    for atk in attacks:
-        key = (atk.attacker.name, atk.attacker.tag)
-        if key in attack_data.keys():
-            # id: str, target_thlvl: int, source_thlvl: int, stars: int, is_outgoing: bool,
-            # time: datetime.datetime
-            id = atk.attacker_tag + ">" + atk.defender_tag
-            atk_obj = models.Attack(id, atk.defender.town_hall, atk.attacker.town_hall,
-                                    atk.stars, True, old_war.end_time.now)
-            attack_data[key].append(atk_obj)
-
-    missed_attacks, registered = database.save_war_attacks(clan_home.tag, clan_home.name, type, total_attacks,
-                                                           attack_data)
-    return missed_attacks, registered
-
-def send_missed_attacks(misses: dict, clantag: str):
-    clanwatch = database.get_clanwatch(clantag)
-    guild = bot.get_guild(clanwatch._guildid)
-    if guild is not None:
-        channel_id = clanwatch._channel_warmiss
-        if channel_id is not None:
-            channel_id = dataformatter.parse_channel_id(channel_id)
-        channel = disnake.utils.get(guild.channels, id=channel_id)
-        if channel is not None:
-            message = "War missed attack for **{} on {}**:\n".format(
-                clanwatch._name, datetime.datetime.now())
-
-            if len(misses) == 0:
-                message += "\tNone, everyone attacked!"
-            else:
-                for k, v in misses.items():
-                    message += "\t" + str(k) + "\t" + str(v) + "\n"
-            return channel, message
-    return None, None
-
-def prepare_wardigest(fromdate, todate, clantag, clanname):
-    # gather missed attacks data
-    war_data = database.find_war_data(clantag, fromdate, todate)
-    if len(war_data) == 0:
-        return None, None, None, None, None
-
-    # gather war data
-    targetfolder = "db/" + clantag
-    Path(targetfolder).mkdir(parents=True, exist_ok=True)
-    # now process the file and extract data
-    clan_war_data, data_missed, data_cwl_missed = dataformatter.parse_war_data(war_data, clantag)
-    msg = "**{} clan war digest between {} and {}**:\n\n **Missed Attacks - Total:** \n".format(
-        clantag + ", " + clanname,
-        fromdate, todate)
-    count = 0
-    for k, v in data_missed.items():
-        msg += "\t" + str(k) + ": " + str(v) + "\n"
-        count += 1
-    if count == 0:
-        msg += "\t(no data)"
-    msg_warmiss = msg + "\n"
-
-    msg = "\n**Missed Attacks - CWL:** \n"
-    count = 0
-    for k, v in data_cwl_missed.items():
-        count += 1
-        msg += "\t" + str(k) + ": " + str(v) + "\n"
-    if count == 0:
-        msg += "\t(no data)"
-    msg_cwlmiss = msg + "\n"
-
-    data_for_plot, clan_summary = clan_war_data.output_clan_war_data(targetfolder)
-    msg = "\n**Clan Overview**:\n"
-    for k, v in clan_summary.items():
-        msg += "\t" + k + ": " + str(v) + "\n"
-    war_overview = msg + "\n"
-
-    figure = data_for_plot.plot(kind='bar', stacked=True).get_figure()
-    figure.savefig(targetfolder + '/clan_war_data.jpg', format='jpg')
-    # now fetch that file and send it to the channel
-    fileB = disnake.File(targetfolder + "/clan_war_data.jpg")
-    war_plot = fileB
-    return msg_warmiss, msg_cwlmiss, war_overview, war_plot, clan_summary
-    # await channel_to.send(file=fileB,
-    #                       content="**Clan war data plot ready for download**:")
-
 
 @tasks.loop(hours=12)
 async def check_scheduled_task():
@@ -1215,8 +1087,8 @@ async def check_scheduled_task():
                     channel = disnake.utils.get(guild.channels, id=channel_id)
                     if channel is not None:
                         fromdate = utils.get_season_start()
-                        war_miss, cwl_miss, war_overview, war_plot, summary = prepare_wardigest(fromdate, now, clantag,
-                                                                                                clanwatch._name)
+                        war_miss, cwl_miss, war_overview, war_plot, summary = \
+                            bot_functions.prepare_wardigest(fromdate, now, clantag,clanwatch._name, database)
 
                         if war_miss is None or cwl_miss is None or war_overview is None or war_plot is None:
                             await channel.send("Not enough war data for {}, {}".format(clantag, clanwatch._name))
